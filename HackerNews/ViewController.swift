c0409625912ac74e19d5beac4546743bab249b11
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2017-01-06 16:06:55 +0300

    Initial Commit

---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Ellina Kuznetcova on 06/01/2017.
+//  Copyright © 2017 Ellina Kuznetcova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
